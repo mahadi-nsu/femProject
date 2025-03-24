d40f01a0bc44ac418d47d041e223b013707b37db
mahadi-nsu <mahadi.hassan@northsouth.edu>
2025-03-24 13:21:44 +0600

    feat :migration files added

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/mahadi-nsu/femProject/internal/api"
 	"github.com/mahadi-nsu/femProject/internal/store"
+	"github.com/mahadi-nsu/femProject/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,11 @@ func NewApplication() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)

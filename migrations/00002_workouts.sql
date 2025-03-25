5b1eb1889e04df4bef54093d89cca92f503892b2
mahadi-nsu <mahadi.hassan@northsouth.edu>
2025-03-25 10:19:35 +0600

    feat : workout store created along with create workout function

--- migrations/00002_workouts.sql
+++ migrations/00002_workouts.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS workouts (
     description TEXT,
     duration_minutes INTEGER NOT NULL,
     calories_burned INTEGER,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 -- +goose StatementEnd
--- migrations/00003_workout_entries.sql
+++ migrations/00003_workout_entries.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS workout_entries (
     sets INTEGER NOT NULL,
     reps INTEGER,
     duration_seconds INTEGER,
-    weight DECIMAL(5,2)
+    weight DECIMAL(5,2),
     notes TEXT,
     order_index INTEGER NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
